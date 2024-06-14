14857b5c1792f6eea5ac92d3595c7b232220f069
sowmiya2901 <146068072+sowmiya2901@users.noreply.github.com>
2024-06-14 09:16:39 +0530

    Add files via upload

---
+++ academics.php
@@ -0,0 +1,41 @@
+<?php
+include("head.html")
+?>
+<div class="academics">
+<table border="1" height="600px" width="1000px">
+<tr>
+<th>S.NO</th>
+<th>year</th>
+<th>No. of students</th>
+<th>Pass percentage</th>
+</tr>
+<tr>
+<td>1</td>
+<td>I</td>
+<td>600</td>
+<td>97%</td>
+</tr>
+<tr>
+<td>2</td>
+<td>II</td>
+<td>650</td>
+<td>93%</td>
+</tr>
+<tr>
+<td>3</td>
+<td>III</td>
+<td>700</td>
+<td>95%</td>
+</tr>
+<tr>
+<td>4</td>
+<td>IV</td>
+<td>659</td>
+<td>99%</td>
+</tr>
+</table>
+</div>
+
+<?php 
+include("footer.html")
+?>
---
+++ home.php
@@ -0,0 +1,21 @@
+<?php
+include("head.html")
+?>
+<body>
+<div class="home">
+<div class="img">
+<img src="img/clg.jpg" height="300px" width="1000px"/>
+</div>
+<div class="header">
+<h1>SN ENGINEERING COLLEGE</h1>
+</div>
+<div class="para">
+<p>SN Engineering College is a premier and leading Institution located at the heart of the city Salem,which is well connected by road and rail.The Institution strives to mould the students to become Responsible Engineers of our nation.The institution is commited to offer value based education to the students and to develop their technical,intechnical and professional skills to meet the needs and challenges of the society.
+
+</p>
+</div>
+</div>
+</body>
+<?php 
+include("footer.html")
+?>
